cb395c626fb8d6aa3bd0476e6cce4abb240a0459
TianQiu16156328 <61176104+TianQiu16156328@users.noreply.github.com>
2020-03-25 05:44:53 +0800

    ice_fire

---
+++ ice_fire/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+
+
+      <!--[if lt IE 9]>
+         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+         <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>-->
+
+  <!-- Latest compiled Bootstrap JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+	<!-- latest jQuery library -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<!-- jQuery library -->
+  <script src="js/jquery-2.1.4.min.js"></script>
+
+  <title>Song of Ice and Fire</title>
+</head>
+
+
+<body>
+  <?php
+
+  $curl = "https://www.anapioficeandfire.com/api/book";
+
+  $book = json_decode($curl);
+  ?>
+
+  <div class='container'>
+  	<div class='row'>
+  		<div class='col-12 col-md-3'>
+  			<?php
+  				echo '<h3>'.$book->name.'</h3>';
+  				echo '<p><strong>isbn:</strong>'.$book->isbn.'<strong>authors:</strong>'.$book->authors.'<strong>publisher:</strong>'.$book->publisher.'</p>';
+  			?>
+  		</div>
+  	</div>
+  </div>
+
+</body>
+
+</html>
